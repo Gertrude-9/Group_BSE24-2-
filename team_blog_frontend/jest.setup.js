41863230ba5b527ec92328108d0f8d59911a9eb4
Liven-Allan <lutaloallan6@gmail.com>
2025-09-20 11:01:37 +0300

    fixing build error

--- team_blog_frontend/jest.setup.js
+++ team_blog_frontend/jest.setup.js
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+
+// Mock import.meta.env for Jest
+Object.defineProperty(global, 'import', {
+  value: {
+    meta: {
+      env: {
+        VITE_API_URL: 'http://127.0.0.1:8000'
+      }
+    }
+  }
+});
