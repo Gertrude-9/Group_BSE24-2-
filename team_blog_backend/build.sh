059bd254b3da8fde437e4761d1aa536caa2fcbd2
Lutalo Allan <126987883+Liven-Allan@users.noreply.github.com>
2025-09-21 21:29:46 +0300

    Liven (#30)
    * added vercel
    
    * updated urls.py
    
    * fixed json file for vercel
    
    * modified wsgi.py
    
    * changed root directory
    
    * build initialization in vercel.json
    
    * change in file vercel.json
    
    * further update to vercel.json
    
    * used python3
    
    * added content to build.sh
    
    * added procfile
    
    * modified procfile
    
    * updated requirements.txt
    
    * Update requirements.txt
    
    * modified build.sh
    
    * fixed directory
    
    * added sqlite data file

--- team_blog_backend/build.sh
+++ team_blog_backend/build.sh
@@ -10,3 +10,7 @@ python manage.py collectstatic --no-input
 
 # Run Django migrations
 python manage.py migrate --no-input
+
+# Load data from the fixture file
+python manage.py loaddata blog_app_data.json
+
