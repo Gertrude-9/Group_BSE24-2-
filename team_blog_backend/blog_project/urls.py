8f1cb0666d57bd2b0b74fe4e8517365e19768977
Liven-Allan <lutaloallan6@gmail.com>
2025-09-21 12:49:53 +0300

    updated urls.py

--- team_blog_backend/blog_project/urls.py
+++ team_blog_backend/blog_project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('blog_app.urls')),
+    path('', include('blog_app.urls')),
 ]
 
 # Serve media files during development
