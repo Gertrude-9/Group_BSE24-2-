a66d89f3e4401318fb945ad2c26835f67d121d17
Liven-Allan <lutaloallan6@gmail.com>
2025-09-21 14:45:14 +0300

    added content to build.sh

--- team_blog_backend/build.sh
+++ team_blog_backend/build.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-python manage.py collectstatic --noinput --clear
+echo "Starting migrations..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+echo "Migrations complete."
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear
+echo "Static files collected."
