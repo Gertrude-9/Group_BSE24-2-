2a34e5b162ee12d33055e20714ac1c97cc0b75a2
Liven-Allan <lutaloallan6@gmail.com>
2025-09-21 21:25:03 +0300

    added sqlite data file

--- team_blog_backend/build.sh
+++ team_blog_backend/build.sh
@@ -5,5 +5,12 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
+# Collect static files
+python manage.py collectstatic --no-input
+
 # Run Django migrations
-python manage.py migrate --no_input
+python manage.py migrate --no-input
+
+
+# Load data from the fixture file
+python manage.py loaddata blog_app_data.json
